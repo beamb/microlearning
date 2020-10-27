37de3b347bb815f314ca746d4fde1f1b2e718bbe
beamb <60703644+beamb@users.noreply.github.com>
2020-10-27 20:33:58 +0100

    Revert "Extension setup"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { firebaseAppAuth } from "./firebase";
-import { LoggedInPrompt } from "./LoggedInPrompt";
-import { NotLoggedInPrompt } from "./NotLoggedInPrompt";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App = (props) => {
-  // We're using a package where someone else created a "hook" for using Firebase functionality
-  const [user, loading] = useAuthState(firebaseAppAuth);
-  
-  // If it's still loading the user-state, we're showing nothing here. We could show a spinner,
-  // but it will change very fast and it might be more confusing than a blank screen for half a sec
-  if (loading) {
-    return (
-    <p>Loading...</p>
-    );
-  }
-
-  // We will show a component based on whether we have a "user" or not
-  return user ? <LoggedInPrompt /> : <NotLoggedInPrompt />;
-};
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;

fd355ace1eb5994656c347dd1297a4fc2c3d09a9
Beatrice Ambrosi de Magistris Verzier <beam@itu.dk>
2020-11-18 23:41:11 +0100

    minor changes in Quiz

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Language from "./Language";
-import { withRouter } from "react-router-dom";
 import { javaQuestions } from "./javaquestions";
 import { pythonQuestions } from "./pythonquestions";
 import { javascriptQuestions } from "./javascriptquestions";

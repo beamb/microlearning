6ed38a57c1dccc15e77c8499cda0130bcf377c62
anneschjoedt <anne.schjoedt@gmail.com>
2020-12-17 18:09:04 +0100

    created MuiTheme

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -2,6 +2,8 @@ import React from "react";
 import Stepper from "@material-ui/core/Stepper";
 import Step from "@material-ui/core/Step";
 import StepLabel from "@material-ui/core/StepLabel";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
 
 const ProgressBar = ({ numberOfQuestions, skipped, activeStep }) => {
   const steps = Array.from(Array(numberOfQuestions).keys());
@@ -10,20 +12,37 @@ const ProgressBar = ({ numberOfQuestions, skipped, activeStep }) => {
     return skipped.has(step);
   };
 
+  const theme = createMuiTheme({
+    overrides: {
+      MuiStepIcon: {
+        root: {
+          "&$completed": {
+            color: "#a5d6a7",
+          },
+          "&$active": {
+            color: "#21B6A8",
+          },
+        },
+      },
+    },
+  });
+
   return (
-    <Stepper activeStep={activeStep} orientation="vertical">
-      {steps.map((index) => {
-        const stepProps = {};
-        if (isStepWrong(index)) {
-          stepProps.completed = false;
-        }
-        return (
-          <Step key={index} {...stepProps}>
-            <StepLabel></StepLabel>
-          </Step>
-        );
-      })}
-    </Stepper>
+    <ThemeProvider theme={theme}>
+      <Stepper activeStep={activeStep} orientation="vertical">
+        {steps.map((index) => {
+          const stepProps = {};
+          if (isStepWrong(index)) {
+            stepProps.completed = false;
+          }
+          return (
+            <Step key={index} {...stepProps}>
+              <StepLabel></StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+    </ThemeProvider>
   );
 };
 
